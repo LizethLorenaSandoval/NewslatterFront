34fd8c262a48eba9b17b11537c8e3ff3145b6e88
LizethLorenaSandoval <lizeth.sandoval@btgpactual.com>
2023-03-15 15:28:15 -0500

    se crea vista de registrarse

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 import { LoginComponent } from './componentes/login/login.component';
+import { RegistrarseComponent } from './componentes/registrarse/registrarse.component';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent},
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
-  {path: 'index', component: AppComponent}
+  {path: 'index', component: LoginComponent},
+  {path: 'registrarse', component: RegistrarseComponent},
+
+  // {path: 'not-found', component: NotFoundComponent},
+  // {path: '**', redirectTo: 'not-found'},
 ];
 
 @NgModule({
