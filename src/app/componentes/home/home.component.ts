eef01bde656017b7f5b95dab700736cee6202e99
LizethLorenaSandoval <lizeth.sandoval@btgpactual.com>
2023-03-24 14:03:00 -0500

    se agrega logica de modal en el home

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [NgbModalConfig, NgbModal]
 })
 export class HomeComponent {
+
+
+// logica para el modal
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+		// customize default values of modals used by this component tree
+		config.backdrop = 'static';
+		config.keyboard = false;
+	}
+
+	open(content: any) {
+		this.modalService.open(content);
+	}
 
 }
