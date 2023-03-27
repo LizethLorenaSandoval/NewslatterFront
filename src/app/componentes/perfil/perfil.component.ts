f9e59ce0aadbe6c82076c8af797d26fb2cdfc93a
LizethLorenaSandoval <lizeth.sandoval@btgpactual.com>
2023-03-27 17:13:11 -0500

    se agrega data get de las celulas en mi perfil

--- src/app/componentes/perfil/perfil.component.ts
+++ src/app/componentes/perfil/perfil.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+import { CelulasService } from 'src/app/servicios/celulas/celulas.service';
 
 @Component({
   selector: 'app-perfil',
@@ -8,9 +9,19 @@ import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 })
 export class PerfilComponent {
 
+  // Variables
+	celulas: any = {};
+
+
+  ngOnInit(){
+		this.getCelulas();
+	}
   
 // logica para el modal
-constructor(config: NgbModalConfig, private modalService: NgbModal) {
+constructor(config: NgbModalConfig, 
+  private modalService: NgbModal,
+  private celulasService: CelulasService
+  ) {
   // customize default values of modals used by this component tree
   config.backdrop = 'static';
   config.keyboard = false;
@@ -18,6 +29,14 @@ constructor(config: NgbModalConfig, private modalService: NgbModal) {
 
 open(content: any) { //el size es opcional, para el tamaño de la modal
   this.modalService.open(content);
+}
+
+// Servicios de celulas
+getCelulas(){
+  this.celulasService.getCelulas().subscribe((res) =>{
+      this.celulas = res;
+      console.log(this.celulas);
+  })
 }
 
 }
