2b1b2f4b158a666b608735174c3d28b0ce4de6c9
LizethLorenaSandoval <lizeth.sandoval@btgpactual.com>
2023-03-28 15:18:42 -0500

    se llama data de tipo de documento

--- src/app/componentes/perfil/perfil.component.ts
+++ src/app/componentes/perfil/perfil.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 import { CelulasService } from 'src/app/servicios/celulas/celulas.service';
+import { TipoDocumentoService } from 'src/app/servicios/tipo_documento/tipo-documento.service';
 
 @Component({
   selector: 'app-perfil',
@@ -11,16 +12,19 @@ export class PerfilComponent {
 
   // Variables
 	celulas: any = {};
+  tipo_documento: any = {};
 
 
   ngOnInit(){
+    this.getTipoDoc();
 		this.getCelulas();
 	}
   
 // logica para el modal
 constructor(config: NgbModalConfig, 
   private modalService: NgbModal,
-  private celulasService: CelulasService
+  private celulasService: CelulasService,
+  private TipoDocumentoService: TipoDocumentoService
   ) {
   // customize default values of modals used by this component tree
   config.backdrop = 'static';
@@ -31,12 +35,20 @@ open(content: any) { //el size es opcional, para el tamaño de la modal
   this.modalService.open(content);
 }
 
-// Servicios de celulas
-getCelulas(){
-  this.celulasService.getCelulas().subscribe((res) =>{
-      this.celulas = res;
-      console.log(this.celulas);
-  })
-}
+  // Servicios de celulas
+  getCelulas(){
+    this.celulasService.getCelulas().subscribe((res) =>{
+        this.celulas = res;
+        console.log(this.celulas);
+    })
+  }
+
+  // Servicios de tipo documento
+  getTipoDoc(){
+		this.TipoDocumentoService.getTipoDoc().subscribe((res) =>{
+				this.tipo_documento = res;
+				console.log(this.tipo_documento);
+		})
+	}
 
 }
--- src/app/componentes/registrarse/registrarse.component.ts
+++ src/app/componentes/registrarse/registrarse.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CelulasService } from 'src/app/servicios/celulas/celulas.service';
+import { TipoDocumentoService } from 'src/app/servicios/tipo_documento/tipo-documento.service';
 
 @Component({
   selector: 'app-registrarse',
@@ -10,13 +11,16 @@ export class RegistrarseComponent {
 
   // Variables
 	celulas: any = {};
+  tipo_documento: any = {};
 
   ngOnInit(){
+    this.getTipoDoc();
 		this.getCelulas();
 	}
 
   constructor(
-    private celulasService: CelulasService
+    private celulasService: CelulasService,
+    private TipoDocumentoService: TipoDocumentoService
     ) {}
 
   // Servicios de celulas
@@ -24,6 +28,14 @@ export class RegistrarseComponent {
 		this.celulasService.getCelulas().subscribe((res) =>{
 				this.celulas = res;
 				console.log(this.celulas);
+		})
+	}
+
+  // Servicios de tipo documento
+  getTipoDoc(){
+		this.TipoDocumentoService.getTipoDoc().subscribe((res) =>{
+				this.tipo_documento = res;
+				console.log(this.tipo_documento);
 		})
 	}
 }
